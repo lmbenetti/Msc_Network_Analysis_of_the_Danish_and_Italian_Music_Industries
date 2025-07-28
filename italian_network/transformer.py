e31c124bcf67f8c4b212a4cb1d3c00d8237d8ef8
limb <limb@itu.dk>
2025-07-28 11:58:03 +0200

    deleted file

--- italian_network/transformer.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/Users/licho/Documents/ITU/Msc/Msc_Network_Analysis_of_the_Danish_and_Italian_Music_Industries/italian_network/projection_band_full.txt", sep="\t")
-
-df.to_csv("/Users/licho/Documents/ITU/Msc/Msc_Network_Analysis_of_the_Danish_and_Italian_Music_Industries/italian_network/projection_band_full.tsv", sep='\t', index=False)
